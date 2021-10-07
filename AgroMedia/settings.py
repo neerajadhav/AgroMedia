f59f6e87749053e0497eba7a1cce59aaeadad394
neerajadhav <adhavneeraj9500@gmail.com>
2021-10-08 05:14:26 +0530

    backup 3

--- AgroMedia/settings.py
+++ AgroMedia/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'landing',
     'home',
+    'posts',
     'crispy_forms',
 ]
 
--- home/models.py
+++ home/models.py
@@ -23,6 +23,26 @@ class Profile(models.Model):
 
     def get_friends_count(self):
         return self.friends.count()
+    
+    def get_posts_no(self):
+        return self.posts.all().count()
+    
+    def get_all_authors_posts(self):
+        return self.posts.all()
+
+    def get_likes_given_no(self):
+        likes = self.like_set.all()
+        total_liked = 0
+        for item in likes:
+            total_liked += 1
+        return total_liked
+
+    def get_likes_received_no(self):
+        posts = self.posts.all()
+        total_liked = 0
+        for item in posts:
+            total_liked += item.liked.all().count()
+        return total_liked
 
     def __str__(self):
         return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')} Profile"
